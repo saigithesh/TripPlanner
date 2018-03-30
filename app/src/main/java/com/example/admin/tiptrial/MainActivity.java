9a3ad193548d848ed87a858d6ab455bedd9000f2
AshRk1997 <reachashrk@gmail.com>
2018-03-30 17:56:05 +0530

    This is the app

--- app/src/main/java/com/example/admin/tiptrial/MainActivity.java
+++ app/src/main/java/com/example/admin/tiptrial/MainActivity.java
@@ -13,31 +13,45 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.AccelerateInterpolator;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.animation.BounceInterpolator;
 import android.widget.ImageView;
 
 /*<div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>*/
 public class MainActivity extends AppCompatActivity {
     private PreferenceManager prefManager;
+    ImageView i1,i2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         prefManager = new PreferenceManager(this);
+       /* i1 = (ImageView) findViewById(R.id.imageView);
+        i2 = (ImageView) findViewById(R.id.imageView2);*/
 
     }
     @Override
     protected void onStart() {
         super.onStart();
+
         startAnimation();
+        /*float b1 = getResources().getDisplayMetrics().heightPixels - (i1.getHeight());
+        float b2 = getResources().getDisplayMetrics().heightPixels - (i2.getHeight());
+        float b = b1 - b2;
+        i1.animate()
+                .translationY(b)
+.setInterpolator(new AccelerateInterpolator())
+                .setInterpolator(new BounceInterpolator())
+                .setDuration(2000);*/
         final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 int Permission_All = 1;
 
-                String[] Permissions = {Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, };
+                String[] Permissions = {Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET, Manifest.permission.ACCESS_NETWORK_STATE};
                 if(!hasPermissions(MainActivity.this, Permissions)){
                     ActivityCompat.requestPermissions(MainActivity.this, Permissions, Permission_All);
                 }
@@ -50,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
                     launchNextScreen();
                 }
             }
-        }, 2000);
+        }, 1970);
 
 
 
@@ -111,4 +125,5 @@ public class MainActivity extends AppCompatActivity {
         }
         return true;
     }
+
 }
--- app/src/main/java/com/example/admin/tiptrial/MapsActivity.java
+++ app/src/main/java/com/example/admin/tiptrial/MapsActivity.java
@@ -2,21 +2,29 @@ package com.example.admin.tiptrial;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.widget.EditText;
 
-import com.example.admin.tiptrial.GPSTracker;
-import com.example.admin.tiptrial.R;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,32 +35,54 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private GPSTracker gpsTracker;
     private Location mLocation;
     double latitude, longitude;
-
+    double latitude2, longitude2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        mapfunction();
+    }
 
-        gpsTracker = new GPSTracker(getApplicationContext());
-        mLocation = gpsTracker.getLocation();
+    private void mapfunction() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
 
-        checkPermissions();
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+            gpsTracker = new GPSTracker(getApplicationContext());
+            mLocation = gpsTracker.getLocation();
+
+            latitude = mLocation.getLatitude();
+            longitude = mLocation.getLongitude();
+
+            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+        } else {
+            checkPermissions();
+        }
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        LatLng my_loc = new LatLng(latitude, longitude);
+        mMap.addMarker(new MarkerOptions().position(my_loc).title("I'm here...").icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_person_black_24dp)));
+        /*mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(latitude, longitude);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("I'm here..."));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(my_loc, 13));
+        mMap.animateCamera(CameraUpdateFactory.zoomIn());
+
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(my_loc)      // Sets the center of the map to location user
+                .zoom(17)                   // Sets the zoom
+                .bearing(90)                // Sets the orientation of the camera to east
+                .tilt(30)                   // Sets the tilt of the camera to 30 degrees
+                .build();                   // Creates a CameraPosition from the builder
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
     }
+
 
     public void checkPermissions() {
         int permissionLocation = ContextCompat.checkSelfPermission(MapsActivity.this,
@@ -65,8 +95,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), REQUEST_ID_MULTIPLE_PERMISSIONS);
             }
         } else {
-            latitude = mLocation.getLatitude();
-            longitude = mLocation.getLongitude();
+            mapfunction();
         }
 
     }
@@ -77,8 +106,48 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         int permissionLocation = ContextCompat.checkSelfPermission(MapsActivity.this,
                 Manifest.permission.ACCESS_FINE_LOCATION);
         if (permissionLocation == PackageManager.PERMISSION_GRANTED) {
-            latitude = mLocation.getLatitude();
-            longitude = mLocation.getLongitude();
+            mapfunction();
+        }
+        else
+        {
+            checkPermissions();
         }
     }
+
+    public void onMapSearch(View view) {
+        EditText locationSearch = (EditText) findViewById(R.id.editText);
+        String location = locationSearch.getText().toString();
+        List<Address>addressList = null;
+
+        if (location != null || !location.equals("")) {
+            Geocoder geocoder = new Geocoder(this);
+            try {
+                addressList = geocoder.getFromLocationName(location, 1);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Address address = addressList.get(0);
+latitude2 = address.getLatitude();
+longitude2 = address.getLongitude();
+            LatLng latLng1 = new LatLng(latitude2, longitude2);
+
+            LatLng my_loc = new LatLng(latitude, longitude);
+            Marker from = mMap.addMarker(new MarkerOptions().position(my_loc).title("I'm here...").icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_person_black_24dp)));
+            Marker to =   mMap.addMarker(new MarkerOptions().position(latLng1).title("marker1"));
+            LatLngBounds.Builder builder = new LatLngBounds.Builder();
+            builder.include(to.getPosition());
+            builder.include(from.getPosition());
+            LatLngBounds bounds = builder.build();
+
+            int width = getResources().getDisplayMetrics().widthPixels;
+            int height = getResources().getDisplayMetrics().heightPixels;
+            int padding = (int) (width * 0.10);
+            CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, width, height, padding);
+            mMap.animateCamera(CameraUpdateFactory.zoomOut());
+            mMap.animateCamera(cu);
+        }
+    }
+
+
 }
